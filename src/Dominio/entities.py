db3a5058d27e53bacb5ddaf7231b0f5554efad27
Giann33 <123707923+Giann33@users.noreply.github.com>
2023-04-12 17:01:16 -0600

    update

---
+++ src/Dominio/entities.py
@@ -0,0 +1,12 @@
+class distribuidor:
+    def __init__(self) -> None:
+        self.nombredistribuidor = None
+        self.iddistribuidor = None        
+
+class bodega:
+    def __init__(self) -> None:
+        self.nombrebodega = None
+        self.idbodega = None   
+        self.limitebodega = None   
+
+        
